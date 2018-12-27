157e3fa20d28bd382348e93f25db1dd9ca37feaa
zhegerenhendiao <42993733+zhegerenhendiao@users.noreply.github.com>
2018-12-27 11:15:57 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace pictReader
+{
+    partial class Form1
+    {
+	/// <summary>
+	/// 必需的设计器变量。
+	/// </summary>
+	private System.ComponentModel.IContainer components = null;
+
+	/// <summary>
+	/// 清理所有正在使用的资源。
+	/// </summary>
+	/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+	protected override void Dispose(bool disposing)
+	{
+	    if (disposing && (components != null))
+	    {
+		components.Dispose();
+	    }
+	    base.Dispose(disposing);
+	}
+
+	#region Windows 窗体设计器生成的代码
+
+	/// <summary>
+	/// 设计器支持所需的方法 - 不要修改
+	/// 使用代码编辑器修改此方法的内容。
+	/// </summary>
+	private void InitializeComponent()
+	{
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbInputFolder = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.保存button = new System.Windows.Forms.Button();
+            this.放大button = new System.Windows.Forms.Button();
+            this.缩小button = new System.Windows.Forms.Button();
+            this.SaveFolder = new System.Windows.Forms.TextBox();
+            this.AllFilesbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(445, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(479, 496);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseWeel);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "上一张";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(540, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "下一张";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "文件路径";
+            // 
+            // tbInputFolder
+            // 
+            this.tbInputFolder.Location = new System.Drawing.Point(95, 9);
+            this.tbInputFolder.Name = "tbInputFolder";
+            this.tbInputFolder.Size = new System.Drawing.Size(250, 21);
+            this.tbInputFolder.TabIndex = 5;
+            this.tbInputFolder.Text = "D:\\建设方案\\CD000006\\20181117-1";
+            this.tbInputFolder.TextChanged += new System.EventHandler(this.tbImageFoder_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(351, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "打开...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 12;
+            this.listBox.Location = new System.Drawing.Point(131, 44);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(297, 508);
+            this.listBox.TabIndex = 7;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // 保存button
+            // 
+            this.保存button.Location = new System.Drawing.Point(858, 9);
+            this.保存button.Name = "保存button";
+            this.保存button.Size = new System.Drawing.Size(75, 23);
+            this.保存button.TabIndex = 8;
+            this.保存button.Text = "保存";
+            this.保存button.UseVisualStyleBackColor = true;
+            this.保存button.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // 放大button
+            // 
+            this.放大button.BackColor = System.Drawing.Color.Transparent;
+            this.放大button.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.放大button.Location = new System.Drawing.Point(24, 136);
+            this.放大button.Name = "放大button";
+            this.放大button.Size = new System.Drawing.Size(75, 23);
+            this.放大button.TabIndex = 9;
+            this.放大button.Text = "放大";
+            this.放大button.UseVisualStyleBackColor = false;
+            this.放大button.Click += new System.EventHandler(this.放大button_Click);
+            // 
+            // 缩小button
+            // 
+            this.缩小button.Location = new System.Drawing.Point(24, 207);
+            this.缩小button.Name = "缩小button";
+            this.缩小button.Size = new System.Drawing.Size(75, 23);
+            this.缩小button.TabIndex = 10;
+            this.缩小button.Text = "缩小";
+            this.缩小button.UseVisualStyleBackColor = true;
+            this.缩小button.Click += new System.EventHandler(this.缩小button_Click);
+            // 
+            // SaveFolder
+            // 
+            this.SaveFolder.Location = new System.Drawing.Point(640, 9);
+            this.SaveFolder.Name = "SaveFolder";
+            this.SaveFolder.Size = new System.Drawing.Size(212, 21);
+            this.SaveFolder.TabIndex = 11;
+            this.SaveFolder.Text = "D:\\MJDNN\\edge\\images\\train";
+            // 
+            // AllFilesbutton
+            // 
+            this.AllFilesbutton.Location = new System.Drawing.Point(24, 61);
+            this.AllFilesbutton.Name = "AllFilesbutton";
+            this.AllFilesbutton.Size = new System.Drawing.Size(75, 23);
+            this.AllFilesbutton.TabIndex = 12;
+            this.AllFilesbutton.Text = "文件列表";
+            this.AllFilesbutton.UseVisualStyleBackColor = true;
+            this.AllFilesbutton.Click += new System.EventHandler(this.AllFilesbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 552);
+            this.Controls.Add(this.AllFilesbutton);
+            this.Controls.Add(this.SaveFolder);
+            this.Controls.Add(this.缩小button);
+            this.Controls.Add(this.放大button);
+            this.Controls.Add(this.保存button);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tbInputFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+	}
+
+	#endregion
+
+	private System.Windows.Forms.PictureBox pictureBox1;
+	private System.Windows.Forms.Button button1;
+	private System.Windows.Forms.Button button2;
+	private System.Windows.Forms.Label label1;
+	private System.Windows.Forms.TextBox tbInputFolder;
+	private System.Windows.Forms.Button button3;
+	private System.Windows.Forms.ListBox listBox;
+	private System.Windows.Forms.Button 保存button;
+	private System.Windows.Forms.Button 放大button;
+	private System.Windows.Forms.Button 缩小button;
+	private System.Windows.Forms.TextBox SaveFolder;
+	private System.Windows.Forms.Button AllFilesbutton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace pictReader
+{
+    public partial class Form1 : Form
+    {
+	//private Bitmap fbt = null;  //原始图片
+	private int n = 0;  //放大缩小倍数
+	private bool isFD = true;//上次操作是否是放大
+	private string nowFilePath = "";    //普通模式当前文件路径
+	private bool isHDP = false;//是否是放灯片模式
+	public Form1()
+	{
+	    InitializeComponent();
+	}
+	//随机生成字母
+	private static char[] constant =
+       {
+	    '0','1','2','3','4','5','6','7','8','9',
+	    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+	    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+       };
+	public static string GenerateRandom(int Length)
+	{
+	    System.Text.StringBuilder newRandom = new System.Text.StringBuilder(52);
+	    Random rd = new Random();
+	    for (int i = 0; i < Length; i++)
+	    {
+		newRandom.Append(constant[rd.Next(52)]);
+	    }
+	    return newRandom.ToString();
+	}
+	
+	//获得文件夹内所有文件名
+	private static void getDirectory(StreamWriter sw,string path)
+	{
+	    DirectoryInfo root = new DirectoryInfo(path);
+	    foreach(FileInfo f in root.GetFiles())
+	    {
+		sw.WriteLine(f.Name);
+	    }
+	    sw.Close();
+	}
+	public static void write2txt(string path)
+	{
+
+	    StreamWriter sw = new StreamWriter(new FileStream("filelist.txt", FileMode.Create));
+	    getDirectory(sw, path);
+	}
+	//txt写入listbox
+	private void Write2Listbox(ListBox lst,string path)
+	{
+	    StreamReader sr = null;
+	    sr = new StreamReader(path, System.Text.Encoding.Default);
+	    string line;
+	    while ((line = sr.ReadLine()) != null)
+	    {
+		lst.Items.Add(line);
+	    }
+	    sr.Close();
+	}
+
+
+	//图像放大
+	private void fd(Bitmap img)
+	{
+	    if (!isFD)
+	    {
+		this.n += 2;
+	    }
+	    this.isFD = true;
+	    if (this.pictureBox1.Image != null)
+	    {
+		int width = img.Width;
+		int height = img.Height;
+
+		Bitmap bt;
+		if(n>0)
+		{
+		    if(n<30)
+		    {
+			bt = new Bitmap(img, (int)(width * (1 + this.n * 0.1)), (int)(height * (1 + this.n * 0.1)));
+			this.pictureBox1.Image = bt;
+		    }
+		    else
+		    {
+			bt = new Bitmap(img, (int)(width * (1 + 30 * 0.1)), (int)(height * (1 + 30 * 0.1)));
+			this.pictureBox1.Image = bt;
+		    }
+
+		}
+
+		this.n++;
+	    }
+	}
+	//图像缩小
+	private void sx(Bitmap img)
+	{
+	    if (isFD)
+	    {
+		this.n -= 2;
+	    }
+	    this.isFD = false;
+	    if (this.pictureBox1.Image != null)
+	    {
+		int width = img.Width;
+		int height = img.Height;
+		Bitmap bt;
+		if (0 > n )
+		{
+		    if (n > -10) 
+		    {
+			bt = new Bitmap(img, (int)(width * (1 + this.n * 0.1)), (int)(height * (1 + this.n * 0.1)));
+			this.pictureBox1.Image = bt;
+		    }else
+		    {
+			bt = new Bitmap(img, (int)(width * (1 - 10 * 0.1)), (int)(height * (1 - 10 * 0.1)));
+			this.pictureBox1.Image = bt;
+		    }
+		}
+		
+		
+		this.n--;
+	    }
+	}
+	private void button1_Click(object sender, EventArgs e)
+	{
+	    
+	}
+
+	private void pictureBox1_MouseEnter(object sender, EventArgs e)
+	{
+	    this.pictureBox1.Focus();
+	}
+
+	protected void pictureBox1_MouseWeel(object sender, MouseEventArgs e)
+	{
+	    Image img = pictureBox1.Image;
+	    Bitmap bmp = new Bitmap(img);
+	    if (!this.isHDP)
+	    {
+		//鼠标向上滚
+		if (e.Delta == 120)
+		{
+		    this.fd(bmp);
+		}
+		else if (e.Delta == -120)//鼠标向下滚
+		{
+		    this.sx(bmp);
+		}
+	    }
+	}
+
+	private void button3_Click(object sender, EventArgs e)
+	{
+	    OpenFileDialog ofd = new OpenFileDialog();
+	    ofd.InitialDirectory = tbInputFolder.Text;
+	    ofd.Filter = "照片|*.jpg;*.png;*.ico;*.bmp;*.tiff;*.tif";
+	    ofd.Title = "打开文件";
+	    DialogResult dr = ofd.ShowDialog();
+	    if(dr == DialogResult.OK)
+	    {
+		this.pictureBox1.ImageLocation = ofd.FileName;
+		this.Text = ofd.FileName;
+		this.nowFilePath = ofd.FileName;
+		tbInputFolder.Text = System.IO.Path.GetDirectoryName(ofd.FileName);
+	    }
+	}
+
+	private void button1_Click_1(object sender, EventArgs e)
+	{
+	    if (this.pictureBox1.Image!=null)
+	    {
+		DirectoryInfo di = new DirectoryInfo(Path.GetDirectoryName(this.nowFilePath));
+		FileInfo[] files1 = di.GetFiles("*.jpg");
+		FileInfo[] files2 = di.GetFiles("*.png");
+		FileInfo[] files3 = di.GetFiles("*.bmp");
+		FileInfo[] files4 = di.GetFiles("*.ico");
+        FileInfo[] files5 = di.GetFiles("*.tiff");
+        FileInfo[] files = new FileInfo[files1.Length + files2.Length + files3.Length + files4.Length + files5.Length];
+		files1.CopyTo(files, 0);
+		files2.CopyTo(files, files1.Length);
+		files3.CopyTo(files, files1.Length + files2.Length);
+		files4.CopyTo(files, files1.Length + files2.Length + files3.Length);
+        files5.CopyTo(files, files1.Length + files2.Length + files3.Length + files4.Length);
+        int i = 0;
+		foreach (FileInfo fi in files)
+		{
+		    if (fi.Name == Path.GetFileName(this.nowFilePath))
+			break;
+		    i++;
+		}
+		this.nowFilePath = i == 0 ? files[files.Length - 1].FullName : files[i - 1].FullName;
+		this.Text = i == 0 ? Path.GetDirectoryName(this.nowFilePath) + "\\" + files[files.Length - 1] : Path.GetDirectoryName(this.nowFilePath) + "\\" + files[i - 1] ;
+		this.pictureBox1.ImageLocation = this.nowFilePath;
+	    }
+	}
+
+	private void button2_Click(object sender, EventArgs e)
+	{
+	    if (this.pictureBox1.Image != null)
+	    {
+		DirectoryInfo di = new DirectoryInfo(Path.GetDirectoryName(this.nowFilePath));
+		FileInfo[] files1 = di.GetFiles("*.jpg");
+		FileInfo[] files2 = di.GetFiles("*.png");
+		FileInfo[] files3 = di.GetFiles("*.bmp");
+		FileInfo[] files4 = di.GetFiles("*.ico");
+        FileInfo[] files5 = di.GetFiles("*.tiff");
+        FileInfo[] files = new FileInfo[files1.Length + files2.Length + files3.Length + files4.Length + files5.Length];
+		files1.CopyTo(files, 0);
+		files2.CopyTo(files, files1.Length);
+		files3.CopyTo(files, files1.Length + files2.Length);
+		files4.CopyTo(files, files1.Length + files2.Length + files3.Length);
+        files5.CopyTo(files, files1.Length + files2.Length + files3.Length + files4.Length);
+		int i = 0;
+		foreach (FileInfo fi in files)
+		{
+		    if (fi.Name == Path.GetFileName(this.nowFilePath))
+			break;
+		    i++;
+		}
+		this.nowFilePath = i == (files.Length - 1) ? files[0].FullName : files[i + 1].FullName;
+		this.Text = i == (files.Length - 1) ? Path.GetDirectoryName(this.nowFilePath) + "\\" + files[0]  : Path.GetDirectoryName(this.nowFilePath) + "\\" + files[i + 1] ;
+		this.pictureBox1.ImageLocation = this.nowFilePath;
+	    }
+	}
+
+	private void tbImageFoder_TextChanged(object sender, EventArgs e)
+	{
+
+	}
+
+	private void pictureBox1_Click(object sender, EventArgs e)
+	{
+
+	}
+
+	private void button4_Click(object sender, EventArgs e)
+	{
+            //   FolderBrowserDialog dlg = new FolderBrowserDialog();
+            //   dlg.SelectedPath = SaveFolder.Text;
+            //   if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            //   foreach (Control ctrl in pictureBox1.Controls)
+            //   {
+            //PictureBox pb = (PictureBox)ctrl;
+            //if (pb.Image != null)
+            //{
+            //    this.pictureBox1.Image.Save( "D:\\"+GenerateRandom(8)+".jpg");
+            //}
+            //   }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "照片|*.jpg;*.png;*.bmp;*.ico";
+            sfd.FileName = SaveFolder.Text + "\\" + GenerateRandom(8);
+            sfd.DefaultExt = "jpg";
+            sfd.AddExtension = true;
+            DialogResult dr = sfd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                if (this.pictureBox1.Image != null)
+                {
+                    this.pictureBox1.Image.Save(sfd.FileName);
+                    this.Text = sfd.FileName;
+                    this.nowFilePath = sfd.FileName;
+                    SaveFolder.Text = System.IO.Path.GetDirectoryName(sfd.FileName);
+                }
+            }
+        }
+
+	private void 放大button_Click(object sender, EventArgs e)
+	{
+	    Image img = pictureBox1.Image;
+	    Bitmap bmp = new Bitmap(img);
+	    this.fd(bmp);
+	}
+
+	private void 缩小button_Click(object sender, EventArgs e)
+	{
+	    Image img = pictureBox1.Image;
+	    Bitmap bmp = new Bitmap(img);
+	    this.sx(bmp);
+	}
+
+	private void checkBox1_CheckedChanged(object sender, EventArgs e)
+	{
+	  
+	}
+
+	private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+	{
+	    string str = listBox.SelectedItem.ToString();
+        string filename = tbInputFolder.Text + "\\" + str;
+        this.Text = filename;
+	    pictureBox1.ImageLocation = filename;
+        
+	}
+
+	private void AllFilesbutton_Click(object sender, EventArgs e)
+	{
+	    listBox.Items.Clear();
+	    string path = tbInputFolder.Text;
+	    write2txt(path);
+	    string name = "filelist.txt";
+	    Write2Listbox(listBox, name);
+	}
+
+	private void Write2list_Click(object sender, EventArgs e)
+	{
+	    listBox.Items.Clear();
+	    string name = "E:\\demo#\\pictReader\\pictReader\\bin\\x64\\Release\\filelist.txt";
+	    Write2Listbox(listBox, name);
+	}
+
+	private void radioButton1_CheckedChanged(object sender, EventArgs e)
+	{
+
+	}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
